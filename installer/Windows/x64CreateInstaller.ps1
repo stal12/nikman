edf42ec8d1d89ffc1d5f31457c0b5db486592e23
Andrea Ballestrazzi <ballestrazziandrea@outlook.com>
2023-11-06 21:47:36 +0100

    improvements

--- installer/Windows/x64CreateInstaller.ps1
+++ installer/Windows/x64CreateInstaller.ps1
@@ -15,6 +15,6 @@ echo "DONE"
 
 # Build the installer for x64-Windows architecture
 Write-Host "[INFO] => Building the installer for x64-Windows architecture..." -NoNewLine
-wix build -o build/NikmanInstaller.msi -src Folders.wxs -src GameComponents.wxs -src Package.wxs -src generated\\Resources.wxs -src generated\\Shaders.wxs -culture en-US -loc Package.en-us.wxl -arch x64
+wix build -o build/NikmanInstaller.msi -src Folders.wxs -src GameComponents.wxs -src Package.wxs -src NoLicenseUI.wxs -src generated\\Resources.wxs -src generated\\Shaders.wxs -culture en-US -loc Package.en-us.wxl -arch x64 -ext WixToolset.UI.wixext
 echo "DONE"
 echo "[INFO] => The installer is located in the `"build`" folder and is named `"NikmanInstaller.msi`""
